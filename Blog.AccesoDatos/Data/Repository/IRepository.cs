e276266061e65760e014973b3213d4f07a7e4ec7
Kevin Alexis Martínez Herrera <alexismtz_99@icloud.com>
2021-12-25 22:54:17 -0600

    first commit

---
+++ Blog.AccesoDatos/Data/Repository/IRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Blog.AccesoDatos.Data.Repository
+{
+    //Indicamos que el generico tienen que ser unicamente clases
+
+    public interface IRepository<T> where T : class
+    {
+        //Métodos CRUD comunes en todas las clases
+        //Unicamente excluimos el método Update debido a que en cada Modelo pueden variar los atributos que se modificaran en la BD
+
+
+        //Obtener por ID
+        T Get(int id);
+
+
+
+        //Obtener todos con o sin parametros de busqueda 
+        IEnumerable<T> GetAll(
+           Expression<Func<T, bool>> filter = null,
+           Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+           string includeProperties = null
+            );
+
+
+        //Obtener primero de una lista con y sin filtrado
+        T FirstOrDefaul(
+             Expression<Func<T, bool>> filter = null,
+             string includeProperties = null
+
+            );
+
+
+        //Añadir registro
+        void Add(T entity);
+       
+        //Remover por id
+        void Remove(int id);
+
+        //Remover por entidad 
+        void Remove(T entity);
+
+
+    }
+}
